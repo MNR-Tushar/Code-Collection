4e07cc9111a2b1d2062a3c0b307e6f01dc6e45d6
MD Naimur Rahman <69746759+MNR-Tushar@users.noreply.github.com>
2024-01-23 23:06:08 +0600

    Create Decimal to Binary.cpp

---
+++ Number Theory/Base Conversion/Decimal to Binary.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll              long long int
+#define     all(v)          v.begin(),v.end()
+#define     Max             1000000000000000014
+#define     Min             -1000000000000000014
+#define     pb              push_back
+#define     PI              3.1415926535897
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int main()
+{
+    optimize();
+    // Decimal to binary;
+
+    int n;
+    cin>>n;
+    string s;
+    while(n!=0)
+    {
+        if(n%2==0)
+        {
+            s+='0';
+        }
+        else s+='1';
+        n/=2;
+    }
+    reverse(s.begin(),s.end());
+    cout <<s <<endl;
+
+
+    return 0;
+}
