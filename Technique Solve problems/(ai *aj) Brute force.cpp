e33bc95ec9fb6b70b3da021137f163cdf57881c7
MD Naimur Rahman <69746759+MNR-Tushar@users.noreply.github.com>
2024-02-11 23:24:18 +0600

    Create (ai *aj) Brute force.cpp

---
+++ Technique Solve problems/(ai *aj) Brute force.cpp
@@ -0,0 +1,45 @@
+
+/*
+Given an array of n integers, find the sum of ai × aj for all 1 ≤ i,j ≤n.
+
+n   n
+∑   ∑  (ai * aj)
+j=1 i=1
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll              long long int
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int main()
+{
+    optimize();
+
+    int n;
+    cin>>n;
+    int a[n+1];
+    for(int i=1;i<=n;i++)cin>>a[i];
+
+    int ans=0;
+
+    for(int i=1;i<=n;i++)
+    {
+      int sum=0;
+       for(int j=1;j<=n;j++)
+       {
+          sum+=a[i]*a[j];
+       }
+
+       ans+=sum;
+
+    }
+    
+    cout <<ans<<endl;
+
+
+    return 0;
+}
+
+
