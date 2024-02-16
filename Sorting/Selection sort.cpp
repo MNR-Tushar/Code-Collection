5c75539deac44cfc2bf14ac7b5af768203881aea
MD Naimur Rahman <69746759+MNR-Tushar@users.noreply.github.com>
2024-02-16 23:33:18 +0600

    Create Selection sort.cpp

---
+++ Sorting/Selection sort.cpp
@@ -0,0 +1,68 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define     ll              long long int
+#define     all(v)          v.begin(),v.end()
+#define     Max             1000000000000000014
+#define     Min             -1000000000000000014
+#define     pb              push_back
+#define     PI              3.1415926535897
+#define endl '\n'
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int main()
+{
+    optimize();
+
+    //Selection sort
+    //Time Complexity O(n*n)
+    
+    int n;
+    cin>>n;  //Array size;
+
+    int array[n];
+    for(int i=0;i<n;i++)
+    {
+       cin>>array[i];
+    }
+
+    for(int i=0;i<n-1;i++) // Outer loop 
+    {
+        int min=i;
+       for(int j=i+1;j<n;j++) // Inner loop
+       {
+          if(array[j]<array[min])
+          {
+             min=j; // min index
+          }
+       }
+       swap(array[i],array[min]);//Swaping two element.
+    }
+
+
+    //Print
+    for(int i=0;i<n;i++)
+    {
+       cout << array[i]<<" ";
+    }
+    cout <<endl;
+
+    return 0;
+
+
+    /*
+
+        Time Complexity Analysis
+        ........................
+        1st Comparisons (n-1)
+        2nd Comparisons (n-2)
+        3rd comparisons (n-3)
+        .
+        .
+        .
+        N-1th Comparisons 1
+
+        = (n-1)+(n-2)+(n-3)+....2+1
+        =(n*(n-1))/2
+        so, Time Complexity O(n*n).
+    */
+}
