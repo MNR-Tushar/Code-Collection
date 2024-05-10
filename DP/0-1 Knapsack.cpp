3be899860521944b9bed8a992906a7b77025bb78
MD Naimur Rahman <69746759+MNR-Tushar@users.noreply.github.com>
2024-05-11 02:26:58 +0600

    Update 0-1 Knapsack.cpp

--- DP/0-1 Knapsack.cpp
+++ DP/0-1 Knapsack.cpp
@@ -1,29 +1,72 @@
+//Please allah help me.
 #include<bits/stdc++.h>
 using namespace std;
-int knapsack(int C, int wt[], int val[], int n){
-    int K[n+1][C+1];
-    for(int i = 0; i<=n; i++) {
-        for(int w = 0; w<=C; w++) {
-            if(i == 0 || w == 0) 
-            {
-                K[i][w] = 0;
-            } 
-            else if(wt[i-1] <= w)
-            {
-                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w]);
-            } 
-            else
-            {
-              K[i][w] = K[i-1][w];
-            }
-        }
+#define     ll              long long int
+#define endl '\n'
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+int t=1;
+const int N=200;
+int dp[N][N];
+int weight[N];
+int value[N];
+void solve()
+{
+    int n,c;
+    cin>>n>>c;
+
+    for(int i=1;i<=n;i++)
+    {
+       cin>>weight[i];
     }
-    return K[n][C];
+
+    for(int i=1;i<=n;i++)
+    {
+       cin>>value[i];
+    }
+    
+    for(int i=0;i<=n;i++)
+    {
+       for(int j=0;j<=c;j++)
+       {
+          if(i==0 or j==0)
+          {
+             dp[i][j]=0;
+          }
+          else if(weight[i]<=j)
+          {
+             dp[i][j]=max(dp[i-1][j],value[i]+dp[i-1][j-weight[i]]);
+          }
+          else
+          {
+             dp[i][j]=dp[i-1][j];
+          }
+       }
+    }
+
+    for(int i=0;i<=n;i++)
+    {
+       for(int j=0;j<=c;j++)
+       {
+         cout << dp[i][j]<<" ";
+       }
+       cout <<endl;
+    }
+
+    cout << "Max profit: "<<dp[n][c]<<endl;
+
 }
-int main(){
-        int val[5] = {4, 3, 6, 5};
-        int wt[5] = {3, 2, 5, 4};
-        int C = 5;
-        int len = sizeof val / sizeof val[0];
-        printf("Maximum Profit achieved:  %d", knapsack(C, wt, val, len));
+int main()
+{
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    
+    //cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+    
+
+
+    return 0;
 }
